d03b81c954d2bca4d3d484a31e373ad41e684338
Z User <z@container>
2026-09-23 15:04:30 +0000

    feat: add initial Prisma migration for Neon Postgres
    - Baseline migration with all 4 tables: ScanRecord, TiemposMuertosInf, Sancion, Indicador
    - Marked as applied (tables already exist via prisma db push)

---
+++ prisma/migrations/20240923_init/migration.sql
@@ -0,0 +1,18 @@
+
+[+] Added Schemas
+  - public
+
+[+] Added tables
+  - ScanRecord
+  - TiemposMuertosInf
+  - Sancion
+  - Indicador
+
+[*] Changed the `ScanRecord` table
+  [+] Added index on columns (codUti, fecha)
+  [+] Added index on columns (fecha)
+  [+] Added index on columns (zonSts)
+  [+] Added index on columns (codAct)
+
+[*] Changed the `TiemposMuertosInf` table
+  [+] Added index on columns (operario)
